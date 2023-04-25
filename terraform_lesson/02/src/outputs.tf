74456edb52a9d4e4b5ddc976f919c5d4af59ede9
Igor-99 <rewin95@mail.ru>
2023-04-25 14:38:44 +0400

    Delete terraform_lesson/02 directory

--- terraform_lesson/02/src/outputs.tf
+++
@@ -1,6 +0,0 @@
-output "instance_external_ip_1" {
-  value = "${yandex_compute_instance.platform.network_interface.0.nat_ip_address}"
-}
-output "instance_external_ip_2" {
-  value = "${yandex_compute_instance.platform_db.network_interface.0.nat_ip_address}"
-}
