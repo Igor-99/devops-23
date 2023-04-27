aa8a22d119910d7c944fb41d5ec4ff53e1bccdcb
Igor-99 <rewin95@mail.ru>
2023-04-27 06:56:01 +0000

    add 04

---
+++ terraform_lesson/04/demonstration1/main.tf
@@ -0,0 +1,55 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">=0.13"
+}
+
+provider "yandex" {
+  token     = var.token
+  cloud_id  = var.cloud_id
+  folder_id = var.folder_id
+  zone      = var.default_zone
+}
+
+
+module "vpc_module" {
+  source = "./vpc_module"
+  vpc_name = "develop"
+  vpc_subnet_name = "develop-ru-central1-a"
+  subnets = [
+    { zone = "ru-central1-a", cidr = "10.0.1.0/24" },
+    { zone = "ru-central1-b", cidr = "10.0.2.0/24" },
+    { zone = "ru-central1-c", cidr = "10.0.3.0/24" },
+  ]
+}
+
+module "test-vm" {
+  source          = "git::https://github.com/udjin10/yandex_compute_instance.git?ref=main"
+  env_name        = "develop"
+  network_id      = module.vpc_module.vpc.id
+  subnet_zones    = ["ru-central1-a"]
+  subnet_ids      = [ module.vpc_module.subnet.id ]
+  instance_name   = "web"
+  instance_count  = 2
+  image_family    = "ubuntu-2004-lts"
+  public_ip       = true
+  
+  metadata = {
+      user-data          = data.template_file.cloudinit.rendered #Для демонстрации №3
+      serial-port-enable = 1
+  }
+
+}
+
+#Пример передачи cloud-config в ВМ для демонстрации №3
+data "template_file" "cloudinit" {
+ template = file("./cloud-init.yml")
+ 
+ vars = {
+   ssh_authorized_keys = local.server_ssh_key
+  } 
+}
+
---
+++ terraform_lesson/04/demonstration1/vpc_module/variables.tf
@@ -0,0 +1,20 @@
+variable "vpc_name" {
+  type        = string
+  default     = "develop"
+  description = "VPC network&subnet name"
+}
+
+#variable "vpc_subnet_name" {
+#  type = string
+#  default = "develop-ru-central1-a"
+#}
+
+#variable "zone" {
+#  type = string
+#  default = "ru-central1-a"
+#}
+
+#variable "cidr" {
+#  type = list
+#  default = ["10.0.1.0/24"]
+#}
